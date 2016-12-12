6eaf67af9f2b16481fa2e1b53112f4d2ca958bb3
jaris <1020392881@qq.com>
2016-12-12 21:40:08 +0800

    gittest

--- gittest/src/main/java/net/jaris/gittest/MainActivity.java
+++ gittest/src/main/java/net/jaris/gittest/MainActivity.java
@@ -1,10 +1,9 @@
-package com.example.githubtest;
+package net.jaris.gittest;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
